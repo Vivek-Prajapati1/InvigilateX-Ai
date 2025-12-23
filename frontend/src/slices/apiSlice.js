b0eeb303f56222a29105332306e32fcf50fc4723
Gulshan Kumar <gulshan311604@gmail.com>
2025-12-23 22:33:39 +0530

    Update

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,15 +1,12 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-// Use Render backend in production, proxy in development
 const baseQuery = fetchBaseQuery({
-  baseUrl: import.meta.env.PROD ? 'https://invigilatex-ai.onrender.com' : '/',
+  baseUrl: import.meta.env.VITE_BACKEND_API_URL,
   credentials: 'include',
 });
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ['User'],
-  // it like a prent to other api
-  // it a build in builder
   endpoints: (builder) => ({}),
 });
